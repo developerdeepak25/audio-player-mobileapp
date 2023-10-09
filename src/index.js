86fd5126ef97095e6aae94fd34e12e77b64cc883
developer_deepak <developer.deepak25@gmail.com>
2023-10-09 20:15:13 +0530

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Router basename={process.env.PUBLIC_URL || "/expressedge"}>
+    <Router basename={process.env.PUBLIC_URL || "/harmonisync"}>
       <App />
     </Router>
   </Provider>
